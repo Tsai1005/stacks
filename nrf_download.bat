ccd2f5a184986bc57c97cfab302a461e2273985d
Tsai1005 <change.tsai@gmail.com>
2020-07-03 09:49:13 +0800

    [CIS]:更新CIS 流程；

---
+++ nrf_download.bat
@@ -0,0 +1,20 @@
+
+@echo "**************************"
+@echo "--------[Nordic]----------"
+@echo "**************************"
+
+set TARGET=-s 682603958
+REM  set TARGET=-s 682798575
+REM  set TARGET=
+
+echo "<eraseall>..."
+nrfjprog --eraseall -f nrf52 %TARGET%
+
+echo "<program>...[%1]"
+nrfjprog --program %1 -f nrf52 %TARGET%
+
+echo "<reset>..."
+nrfjprog --reset -f nrf52 %TARGET%
+
+echo "**************************"
+
